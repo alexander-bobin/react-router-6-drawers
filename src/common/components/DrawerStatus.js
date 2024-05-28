3bcb1002d7bc3b904b6db2a46a05429149747d27
Alexander Bobin <alexander.bobin@gmail.com>
2024-05-28 16:07:35 +1000

    Change color of DrawerStatus

--- src/common/components/DrawerStatus.js
+++ src/common/components/DrawerStatus.js
@@ -4,8 +4,11 @@ import { DrawerPermanenceContext } from "./DrawerPermanenceContext"
 
 function DrawerStatus () {
   const { areDrawersTemporary } = useContext(DrawerPermanenceContext)
+  const colorClasses = areDrawersTemporary
+    ? 'bg-slate-100 border-slate-400 text-slate-800'
+    : 'bg-cyan-100 border-cyan-400 text-cyan-800'
   return (
-    <div className="bg-cyan-100 border-cyan-400 border-4 p-3 text-cyan-800 rounded-lg">
+    <div className={`border-4 p-3 rounded-lg ${colorClasses}`}>
       <p>
         Drawers are <strong>{areDrawersTemporary ? 'temporary' : 'permanent'}</strong>.
         &nbsp;<Link to={`/.?areDrawersTemporary=${areDrawersTemporary ? '0' : '1'}`} className="text-cyan-600 hover:underline">Change</Link>
